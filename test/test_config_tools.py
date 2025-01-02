26052314fce95a6c0fb731607357bdde95966b3e
karltrowbridge1 <karl.trowbridge12@gmail.com>
2025-01-01 23:28:02 -0500

    Began testing
    Also removed some old files that do not need to be here.

---
+++ test/test_config_tools.py
@@ -0,0 +1,73 @@
+import unittest
+from unittest.mock import mock_open, patch
+import json
+import supporting_scripts.config_tools as ct
+
+class TestConfigTools(unittest.TestCase):
+
+    def test_config_data_valid(self):
+        # Mock JSON content
+        mock_data = '{"key": "value"}'
+        expected_result = {"key": "value"}
+
+        with patch("builtins.open", mock_open(read_data=mock_data)):
+            result = ct.config_data("dummy_path.json")
+
+        self.assertEqual(result, expected_result)
+
+        result = ct.config_data()
+
+    def test_config_data_invalid(self):
+        # Mock JSON content
+        mock_data = '{"key": "value"'
+
+        with patch("builtins.open", mock_open(read_data=mock_data)):
+            with self.assertRaises(json.JSONDecodeError):
+                ct.config_data("dummy_path.json")
+
+    def test_getSpotifyID(self):
+        # Track Links
+        trackLinks = [
+            ["spotifyTrackLink", "https://open.spotify.com/track/2QTDuJIGKUjR7E2Q6KupIh?si=32494c664b10490b"],
+            ["spotifyTrackLinkNoSI", "https://open.spotify.com/track/2QTDuJIGKUjR7E2Q6KupIh"],
+            ["spotifyTrackURI", "spotify:track:2QTDuJIGKUjR7E2Q6KupIh"]
+        ]
+        expectedLinkID = "2QTDuJIGKUjR7E2Q6KupIh"
+
+        # Playlist Links
+        playlistLinks = [
+            ["spotifyPlaylistLink", "https://open.spotify.com/playlist/1qGNevTlHmCimM6AjmFv1u?si=34a16b6f03684936"],
+            ["spotifyPlaylistLinkNoSI", "https://open.spotify.com/playlist/1qGNevTlHmCimM6AjmFv1u"],
+            ["spotifyPlaylistURI", "spotify:playlist:1qGNevTlHmCimM6AjmFv1u"]
+        ]
+        expectedPlaylistID = "1qGNevTlHmCimM6AjmFv1u"
+
+        for link in trackLinks:
+            # Use the link located at index 1
+            result = ct.getSpotifyID(link[1])
+
+            # Test the id at index 1
+            self.assertEqual(result['id'], expectedLinkID)
+
+            # Test the type at index 0
+            if "URI" in link[0]:
+                self.assertEqual(result['type'], None)
+            else: 
+                self.assertEqual(result['type'], "track")
+
+        for link in playlistLinks:
+            # Use the link located at index 1
+            result = ct.getSpotifyID(link['id'])
+
+            # Test the id at index 1
+            self.assertEqual(result['id'], expectedPlaylistID)
+
+            # Test the type at index 0
+            if "URI" in link[0]:
+                self.assertEqual(result['type'], None)
+            else: 
+                self.assertEqual(result['type'], "track")
+
+        
+if __name__ == "__main__":
+    unittest.main()

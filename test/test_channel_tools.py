454698f0c17e01efd3d536d2fda5e79319c0635f
karltrowbridge1 <karl.trowbridge12@gmail.com>
2025-01-11 19:43:30 -0500

    Started Test Channel tools class

---
+++ test/test_channel_tools.py
@@ -0,0 +1,8 @@
+import unittest
+from unittest.mock import mock_open, patch
+import json
+import supporting_scripts.channel_tools as cht
+
+class TestChannelTools(unittest.TestCase):
+    def test
+
